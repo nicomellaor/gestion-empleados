9e4307009aa6298b6d78d2fb069a104c5a614a3d
alumno12.lab36 <alumno12.lab36@cloud.uautonoma.cl>
2024-11-29 12:16:35 -0300

    Ruta del server actualizada

--- src/Functions.jsx
+++ src/Functions.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getAllAxios = async () => {
-    var ruta = "http://localhost:8000/api/employees";
+    var ruta = "https://serious-perri-uautonoma-1d5297c7.koyeb.app/api/employees";
     try {
         const response = await axios.get(ruta);
         return response.data.employee;
@@ -12,7 +12,7 @@ export const getAllAxios = async () => {
 };
 
 export const getAxios = async (nro) => {
-    var ruta = "http://localhost:8000/api/employees";
+    var ruta = "https://serious-perri-uautonoma-1d5297c7.koyeb.app/api/employees";
     try {
         const response = await axios.get(`${ruta}?id=${nro}`);
         return response.data.employee;
@@ -23,21 +23,21 @@ export const getAxios = async (nro) => {
 };
 
 export const postAxios = (employee) => {
-    var ruta = "http://localhost:8000/api/employees";
+    var ruta = "https://serious-perri-uautonoma-1d5297c7.koyeb.app/api/employees";
     axios.post(ruta, employee).then(response => {
         console.log(response);
     })
 };
 
 export const putAxios = (nro, employee) => {
-    var ruta = "http://localhost:8000/api/employees";
+    var ruta = "https://serious-perri-uautonoma-1d5297c7.koyeb.app/api/employees";
     axios.put(`${ruta}?id=${nro}`, employee).then(response => {
         console.log(response);
     })
 };
 
 export const deleteAxios = (nro) => {
-    var ruta = "http://localhost:8000/api/employees";
+    var ruta = "https://serious-perri-uautonoma-1d5297c7.koyeb.app/api/employees";
     axios.delete(`${ruta}?id=${nro}`).then( response => {
         console.log(response);
     })
