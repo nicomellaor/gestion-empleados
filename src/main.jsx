82e2709bfe35a5ad83d6670240c13a713c34d3e3
Nicolás Mella <nicolasmella2004@gmail.com>
2024-12-20 23:32:40 -0300

    Cambio a HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap-icons/font/bootstrap-icons.css"
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 )
